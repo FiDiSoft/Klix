dbc4f23e2cb22d0b999287a9231f76a3b3649458
Fiki Alamsyah <fiki@pop-os.localdomain>
2021-09-29 22:55:39 +0700

    add theme

---
+++ lib/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// theme application
+// Color
+Color primary = const Color(0xff11405D);
+Color accent = const Color(0xffC2D1D9);
+
+// TextStyle
+TextStyle primaryTextStyle = GoogleFonts.poppins(color: primary);
+TextStyle accentTextStyle = GoogleFonts.poppins(color: accent);
+TextStyle blackTextStyle = GoogleFonts.poppins(color: Colors.black);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: Colors.white);
+
+// font weight
+FontWeight ligth = FontWeight.w300;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
