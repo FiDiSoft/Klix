9677fe4d8c5955fc851edac0abae042a3166c4fc
dikaMuharman <dika1254@gmail.com>
2021-10-01 07:31:19 +0700

    add some style and google font package

---
+++ lib/constant/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = Color(0xff123D59);
+Color secondaryColor = Color(0xffC2D1D9);
+Color baseTextColor = Color(0xff010406);
+
+TextStyle headingStyle = GoogleFonts.poppins(
+  color: baseTextColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle bodyTextStyle = GoogleFonts.poppins(
+  color: baseTextColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w300,
+);
+
+TextStyle labelTextStyle = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+);
