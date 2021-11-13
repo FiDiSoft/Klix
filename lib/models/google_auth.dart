196386ccdc69fcea502b715c7c7653e7b72f6b61
Rista F.N <golemmite23@gmail.com>
2021-11-13 14:18:09 +0700

    add scope

--- lib/models/google_auth.dart
+++ lib/models/google_auth.dart
@@ -3,7 +3,8 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 Future<UserCredential> signInWithGoogle() async {
   // Trigger the authentication flow
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  final GoogleSignInAccount? googleUser =
+      await GoogleSignIn(scopes: ['https://mail.google.com/']).signIn();
 
   // Obtain the auth details from the request
   final GoogleSignInAuthentication? googleAuth =
