b5b05a5578b5f1bebf093a8a32efc1e038f3a63c
thatBoss <dikamuharman23@gmail.com>
2021-12-17 13:52:38 +0700

    change from gridbuilder to sliverlist

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,6 @@ class Auth extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     User? user = Provider.of<User?>(context);
-    return (user != null) ? HomePage(user: user) : const OnBoardingPage();
+    return (user != null) ? HomePage() : const OnBoardingPage();
   }
 }
